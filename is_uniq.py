1c61a2b322bd9cc6064c5d3bcc63e4ae0bf5cf87
ganarv <ganarv@u24ea173e15d452.ant.amazon.com>
2019-06-14 17:35:52 +0100

    added is_uniq.py

---
+++ is_uniq.py
@@ -0,0 +1,12 @@
+def noDupe(s):
+    a = {}
+    for char in s:
+        if char in a:
+            return False
+        else:
+            a[char] = 1
+    return True
+
+
+    print(noDupe('sdsdsdsddsdsd'))
+    print(noDupe('dsa'))
