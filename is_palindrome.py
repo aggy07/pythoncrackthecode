ba0d5505d5efb7e39269770c07c7dfb3a26aee1f
Agaranallur Ganesan <ganarv@dca90495b37f.ant.amazon.com>
2019-06-15 12:38:37 +0100

    is_pal.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -7,14 +7,32 @@ def is_pal(s):
         while not s[i].isalnum() and i <j:
             i += 1
         while not s[j].isalnum() and i <j:
-            j -=1
+            j -= 1
         
-        if s[i] == s[j]:
+        if s[i].lower() == s[j].lower():
+            return True
+            i +=1
+            j -=1
+        else:
+            return False
+
+def is_pal_new(s):
+    s = s.lower()
+    s.replace(" ","")
+
+    i = 0 
+    j = len(s) -1 
+    while i < j:
+        if s[i].lower() == s[j].lower():
+            return True
             i += 1
             j -= 1
-            return True
-
-    return False
+        else:
+            return False
+    
         
 
 print(is_pal(s))
+print(is_pal("malayalam"))
+print(is_pal("bam"))
+print(is_pal_new("Was it a cat I saw"))
